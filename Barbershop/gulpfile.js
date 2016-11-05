7766584811cd286801d2db2f4ebc899fbee94d0e
Dinara <ditiviola@gmail.com>
2016-11-05 18:51:05 +0300

    gulp

--- Barbershop/gulpfile.js
+++ Barbershop/gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-	gulp.src('./project/**/*.sass')
+	gulp.src('./**/*.sass')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./project'))
+		.pipe(gulp.dest('.'))
 		.on('error', showError);
 });
 
 gulp.task('watch', function(){
-	gulp.run('sass');
-	gulp.watch('./project/**/*.sass',['sass']).on('error', showError);
+	gulp.start('sass');
+	gulp.watch('./**/*.sass',['sass']).on('error', showError);
 })
 
 function showError(error){
