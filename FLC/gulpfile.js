12b2d145d0720ed68566622efce270ff49dd4bb3
Dinara <ditiviola@gmail.com>
2016-11-20 04:27:50 +0300

    inner-pages

--- FLC/gulpfile.js
+++ FLC/gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-	gulp.src('./**/*.sass')
+	gulp.src('./sass/**/*.sass')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('.'))
+		.pipe(gulp.dest('./public/css'))
 		.on('error', showError);
 });
 
 gulp.task('watch', function(){
 	gulp.start('sass');
-	gulp.watch('./**/*.sass',['sass']).on('error', showError);
+	gulp.watch('./sass/**/*.sass',['sass']).on('error', showError);
 })
 
 function showError(error){
