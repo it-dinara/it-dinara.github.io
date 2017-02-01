bbd8bad9a0f793b9ff86fc306952046a2754d9d8
Dinara <ditiviola@gmail.com>
2017-02-01 15:35:31 +0300

    dfhsdfh

--- tattu/public/js/index.js
+++ tattu/public/js/index.js
@@ -20,3 +20,109 @@ $(document).ready(function() {
 			.html(' ' + result + 'р.');
 	});
 })
+
+
+
+//			всплывающее окно, валидация
+
+$(function(){
+	var $inputText = $('.js-validate');
+
+	$inputText.on('keyup', function(e){
+
+//		$('.js-validate').each(function () {
+
+			var value = $(this).val();
+			var validate = $(this).attr('validate');
+
+			if (validate == 'name') {
+				if (/^[a-zа-яё ]+$/i.test(value)) {
+					$(this).removeClass('error')
+				}else{
+					$(this).addClass('error')
+				}
+			}
+
+			if (validate == 'phone') {
+				if (/^[+0-9 ]+$/.test(value)) {
+					$(this).removeClass('error')
+				}else{
+					$(this).addClass('error')
+				}
+			}
+	});
+});
+
+
+//			слайдер
+$(function(){
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		dots: true,
+		dotsEach: true,
+		nav: false,
+		margin: 10,
+		responsive: {
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	});
+
+});
+
+
+
+
+//			bxslider
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  	slideWidth: 162,
+  	minSlides: 2,
+  	maxSlides: 4,
+  	slideMargin: 0,
+  	nextText:'',
+  	prevText:'',
+  	useCSS: false
+  });
+});
+
+
+
+//			всплывающее окно
+
+
+
+$(function(){
+	$(window).on('hashchange', function(e) {
+		var windowName = window.location.hash.replace('#', '');
+		openWindow(windowName);
+	});
+	
+	function openWindow(windowName) {
+		var $modals = $('.js-modal');
+	
+
+		$('.js-click').on('click', function(e){
+			var $window = $('#' + windowName);
+			$window.addClass('opened');
+
+			$modals
+				.on('click.closeWindow', function (event) {
+					if ( ! $(event.target).closest('.modal-content').length) {
+						window.location.hash = "#";
+					}
+				});
+
+		});
+	};
+});
+
+
