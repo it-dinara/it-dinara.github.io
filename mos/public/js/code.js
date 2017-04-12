2047a4d296001d0e0aaf38f7ce0622a71227424e
Dinara <ditiviola@gmail.com>
2017-04-12 18:49:22 +0300

    dhf

--- mos/public/js/code.js
+++ mos/public/js/code.js
@@ -1,4 +1,4 @@
-  $( function() {
+$( function() {
 	$( "#city" ).selectmenu();
  
 	$( "#files" ).selectmenu();
@@ -9,4 +9,39 @@
 		.addClass( "overflow" );
  
 	$( "#salutation" ).selectmenu();
-  } );
+
+
+
+//			inputmask
+
+	$('.js-num').inputmask("+7(999)999-99-99"); //static mask
+
+
+//		Валидация
+
+	var $inputText = $('.js-validate');
+	
+	$inputText.on('keyup', function(e){
+		var value = $(this).val();
+		var validate = $(this).attr('validate');
+		var validateRegExps = {
+			name: /^[a-zа-я ]+$/i,
+			phone: /^[+\d ]+$/
+		};
+		
+		
+		if (validateRegExps[validate]) {
+			
+			if (validateRegExps[validate].test(value)) {
+				$(this).removeClass('error')
+			}else{
+				$(this).addClass('error')
+			}
+			
+		}
+	});
+
+
+
+
+} );
