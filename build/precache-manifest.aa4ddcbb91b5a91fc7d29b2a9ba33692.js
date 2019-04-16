c595a8adde7583d2e320b795b819ebfbbaf322dd
Usenakunova Dinara <Dinara.Usenakunova@tts.loc>
2019-04-16 20:29:14 +0300

    add new folder

---
+++ build/precache-manifest.aa4ddcbb91b5a91fc7d29b2a9ba33692.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "54d3bdec8ad1010b52dc",
+    "url": "/static/js/main.fe894f1c.chunk.js"
+  },
+  {
+    "revision": "ac5caf68cae231a32c7d",
+    "url": "/static/js/2.7e407178.chunk.js"
+  },
+  {
+    "revision": "54d3bdec8ad1010b52dc",
+    "url": "/static/css/main.3e4a8752.chunk.css"
+  },
+  {
+    "revision": "9b055caf7775f53533191b2904fc956d",
+    "url": "/index.html"
+  }
+];
