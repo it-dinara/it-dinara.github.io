325ccbbc46813bfac60ade18559fad3724f53d84
Dinara <ditiviola@gmail.com>
2019-05-07 15:24:32 +0300

    using error boundaries getDerivedStateFromErro

--- udemy/src/ErrorBoundary/ErrorBoundary.js
+++ udemy/src/ErrorBoundary/ErrorBoundary.js
@@ -6,15 +6,17 @@ class ErrorBoundary extends Component {
 		errorMessage: ''
 	}
 
-	componentDidCatch = (error, info) => {
-		this.setState({hasError: true, errorMessage: error});
-	}
+	static getDerivedStateFromError(error) {
+    // Обновить состояние с тем, чтобы следующий рендер показал запасной UI.
+    return { hasError: true };
+  }
 
 
 
 	render () {
+		console.log(this.state)
 		if(this.state.hasError) {
-			return <h1>{this.state.errorMessage}</h1>;
+			return <h1>{this.state.errorMessage.toString()}</h1>;
 		} else {
 			return this.props.children;
 		}
