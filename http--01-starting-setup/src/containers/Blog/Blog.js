acd92e0f16e0aa02ac1f1bed7003a63974a0cd0b
Dinara <ditiviola@gmail.com>
2019-07-02 21:40:52 +0300

    169

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -8,17 +8,36 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then( response => {
-                this.setState({posts: response.data});
+                const qwe = response.data.slice(0, 4);
+                const updatedPosts = qwe.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max',
+                    }
+                })
+                        console.log(qwe)
+                this.setState({posts: updatedPosts});
             });
     }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post title={post.title} key={post.key}/>
+            return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => this.postSelectedHandler(post.id)}
+                    />
         });
         return (
             <div>
@@ -26,7 +45,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
