7234540331cadcdc09802b38ba43e6a156dd1420
Dinara <ditiviola@gmail.com>
2019-07-18 14:15:15 +0300

    170

--- http--01-starting-setup/src/components/FullPost/FullPost.js
+++ http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -10,6 +10,7 @@ class FullPost extends Component {
 
     componentDidUpdate () {
         if(this.props.id) {
+            console.log('this.state.loadedPost ' + !this.state.loadedPost)
             if( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
                 axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
                 .then(response => {
