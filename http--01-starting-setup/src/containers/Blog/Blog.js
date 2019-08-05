2771bc9bd72940dca9348cce86846fe8bf2ee045
Dinara <ditiviola@gmail.com>
2019-08-05 19:03:04 +0300

    175

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -7,22 +7,26 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then( response => {
-                const qwe = response.data.slice(0, 4);
-                const updatedPosts = qwe.map(post => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
                     return {
                         ...post,
                         author: 'Max',
                     }
                 })
-                        console.log(qwe)
                 this.setState({posts: updatedPosts});
+            })
+            .catch(error => {
+                this.setState({error: true})
             });
     }
 
@@ -31,14 +35,17 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author} 
-                    clicked={() => this.postSelectedHandler(post.id)}
-                    />
-        });
+        let posts = <p stye={{textAlign: 'center'}}>Something went wrong!</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                        key={post.id} 
+                        title={post.title} 
+                        author={post.author} 
+                        clicked={() => this.postSelectedHandler(post.id)}
+                        />
+            });
+        }
         return (
             <div>
                 <section className="Posts">
