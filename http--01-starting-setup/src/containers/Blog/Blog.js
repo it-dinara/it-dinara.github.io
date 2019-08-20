367037294da42acad1825672c108ff38b52ead33
Dinara <ditiviola@gmail.com>
2019-08-20 20:38:33 +0300

    the withRouter HOC

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './Blog.css';
-// import Posts from '../Posts/Posts';
-import { Route } from "react-router-dom";
+import Posts from '../Posts/Posts';
+import NewPost from '../NewPost/NewPost';
+import { Route, Link } from "react-router-dom";
 
 class Blog extends Component {
 
@@ -11,13 +12,17 @@ class Blog extends Component {
 				<header>
 					<nav>
 						<ul>
-							<li><a href="/">Home</a></li>
-							<li><a href="/new-post">New Post</a></li>
+							<li><Link to="/">Home</Link></li>
+							<li><Link to={{
+                                pathname: 'new-post',
+                            }}>New Post</Link></li>
 						</ul>
 					</nav>
 				</header>
-				< Route  path="/" exact render={() => <h1>Home</h1>}/ >
-				< Route  path="/" render={() => <h1>Home</h1>}/ >
+				{/* < Route  path="/" exact render={() => <h1>Home</h1>}/ >
+                < Route  path="/" render={() => <h1>Home 2</h1>}/ > */}
+                < Route  path="/" exact component={Posts}/ >
+                < Route path="/new-post" exact={false} component={NewPost}/ >
 			</div>
 		);
 	}
