223dab7afb0ae1e612714b3454b0d1d9c9652c97
Dinara <ditiviola@gmail.com>
2019-08-23 19:52:22 +0300

    212- Loading Routers Lazily

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from '../Posts/Posts';
-import NewPost from '../NewPost/NewPost';
 import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+import asyncComponent from '../../hoc/AsyncComponent';
+// import NewPost from '../NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+	return import('../NewPost/NewPost');
+})
 
 class Blog extends Component {
 
 	state = {
-		auth: false
+		auth: true
 	}
 
 	render () {
@@ -41,10 +46,10 @@ class Blog extends Component {
 				{/* < Route  path="/" exact render={() => <h1>Home</h1>}/ >
 				< Route  path="/" render={() => <h1>Home 2</h1>}/ > */}
 				<Switch>
-					{this.state.auth ? < Route path="/new-post" component={NewPost}/ > : null}
+					{this.state.auth ? < Route path="/new-post" component={AsyncNewPost}/ > : null}
 					< Route  path="/posts" component={Posts}/ >
-					<Route render={() => <h1>Not Found</h1>}/>
-					{/* <Redirect from="/" to="/posts"/> */}
+					{/* <Route render={() => <h1>Not Found</h1>}/> */}
+					<Redirect from="/" to="/posts"/>
 				</Switch>
 			</div>
 		);
