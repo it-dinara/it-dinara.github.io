71b34f44ce975481598ca3f29d3341b5704fd203
Dinara <ditiviola@gmail.com>
2019-08-26 20:15:20 +0300

    deleted file

--- Angular6/helloapp/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-      
-@Component({
-    selector: 'my-app',
-    template: `<div textContent="text"></div>`
-})
-export class AppComponent { 
-    text = "<h1>Срочно в номер!</h1><p>Марсиане атакуют людей!</p>"
-}
