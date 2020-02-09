9fbf04235c20747831e6537e991d6e14fae8c922
Dinara <ditiviola@gmail.com>
2020-02-09 19:13:53 +0300

    edit path in build module

---
+++ bizzon/build/precache-manifest.072b7b50618f6b52af682f141b357bba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ac47515067a154ca5303e92785ada8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a51defe04e0605f3a29",
+    "url": "/static/css/2.c60feeff.chunk.css"
+  },
+  {
+    "revision": "061c1e38d1ec81bc5a49",
+    "url": "/static/css/main.e9dd08ba.chunk.css"
+  },
+  {
+    "revision": "4a51defe04e0605f3a29",
+    "url": "/static/js/2.cd33a445.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.cd33a445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061c1e38d1ec81bc5a49",
+    "url": "/static/js/main.392abbf4.chunk.js"
+  },
+  {
+    "revision": "590633380007f6e118d2",
+    "url": "/static/js/runtime-main.8e1973a6.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
