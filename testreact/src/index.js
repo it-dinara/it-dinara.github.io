2382bffd77518836ed7ea7eace0838c22547fe37
Dinara <ditiviola@gmail.com>
2020-02-17 23:35:56 +0300

    261 lesson - from redux flow to Combining multiple reducers

--- testreact/src/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
- 
-
- 
-ReactDOM.render(
-  React.createElement('h1', null, 'Привет, мир!'),
-  document.getElementById('app')
-);
