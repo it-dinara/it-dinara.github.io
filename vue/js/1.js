abb2f6b16d6d21e4d0645377a3190ab7afe6258a
Dinara Usenakunova <ditiviola@gmail.com>
2024-11-13 20:50:11 +0300

    folder was restructured

--- vue/js/1.js
+++
@@ -1,8 +0,0 @@
-function addScript(src){
-  var script = document.createElement('script');
-  script.src = src;
-  script.async = false; // чтобы гарантировать порядок
-  document.head.appendChild(script);
-}
-
-addScript('js/2.js'); // выполнятся, как только загрузятся
